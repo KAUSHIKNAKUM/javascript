4f6efe1351b62e9623e141334c96f8c483ef662c
KAUSHIKNAKUM <kaushiknakum290@gmail.com>
2024-07-29 19:18:43 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,6 @@
+"use stict"; // treat all js code as newer virsion 
+
+// alert(3+3) // we are using node js not browser 
+
+console(3+3) // code readability should be high
+console("kaushik")
--- 01_basics/test1.js
+++ 01_basics/test1.js
@@ -2,12 +2,18 @@ const accountId =144553
 let accountPassword1 = "kaushik@gmail.com"
 var accountPassword = "12345"
 accountCity = "mahuva"
+let accountState;
 
 //  accountId = 2   // not allowed
+
+/*
+    prefer not to use var
+    because of issue in block scope and functional scope 
+*/
 
 accountEmail = "kb@gmail.com"
 accountPassword = "21212"
 accountCity = "surat"
 
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity]) 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
